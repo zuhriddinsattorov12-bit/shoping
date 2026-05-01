25125dc38d27b8e4d85a0811775e8c5b856ba0e6
zuhriddin <zuhriddinsattorov12@gmail.com>
2026-05-01 16:12:41 +0900

    feat learn about FullStack

---
+++ notice.js
@@ -0,0 +1,13 @@
+/*
+
+Fullstack = Restuarant 
+
+Frontend Development:
+         - BSSR: (Backend Server Side Rendering) > DTE/ JTE/ EJS
+         - SPA (React/Angular/Vue)
+
+API REQUEST:
+         - Design    > Traditional API |  Rest API | Graph API
+         - Method    > GET | POST 
+         - Structure > header |  body
+*/
