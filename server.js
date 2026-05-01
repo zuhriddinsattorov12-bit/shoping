2451864fcfe8c910ef7355efefb0d5169a711855
zuhriddin <zuhriddinsattorov12@gmail.com>
2026-05-01 17:00:31 +0900

    feat: building simple backend server on nodejs

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 // NodeJs Express NestJs
+const express = require("express");
+const app = express();
+
+// Traditional API
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Rest API
+app.get("/rest", (req, res) => {
+  res.json({ name: "Marco", age: 21, group: "MIT" });
+});
+
+app.listen(3000, () => {
+  console.log("Backend Server is running on port 3000");
+});
